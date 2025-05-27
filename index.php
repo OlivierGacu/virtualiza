8660d6edcfdf5c3e63a2d357f3b35e92309bd6bb
OlivierGacu <resistancebandzs@gmail.com>
2025-05-27 02:27:20 +0200

    first push

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Kigali Travels Deployment Case Study</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <?php
+  include('header.html');
+  include('body.html');
+  include('index.htm');
+  include('footer.html');
+  ?>
+</body>
+</html>
